69b00d5cc7270f44ef7f5ee537087afe7914cf08
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-02-14 10:28:04 -0800

    update seed.sql with table insert values. initialize burger_db in mysql workbench.

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,6 @@ USE burger_db;
 CREATE TABLE burger (
     id INT AUTO_INCREMENT,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL 
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY(id) 
 )
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE burger_db;
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Big Mac", 0);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Quarter Pounder with Cheese", 0);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Cookout Burger", 0);
