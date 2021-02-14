9bb2ba220465775592940a87cd11310b769bbcd2
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-02-14 11:06:37 -0800

    add complete file structure. add pseudocode for controller and burger modules.

---
+++ controllers/burger_controllers.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const burger = require('../models/burger')
+
+
+
+// create router for this app, export at end of file.
---
+++ models/burger.js
@@ -0,0 +1,4 @@
+const orm = require('../Config/orm');
+
+
+// code that will call orm functions using burger specific input//
