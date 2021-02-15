3623017d17341f66a131094242cb5dff21a65c13
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-02-14 21:40:08 -0800

    complete selectAll, updateOne, and insertOne orm functions.

--- Config/orm.js
+++ Config/orm.js
@@ -3,16 +3,28 @@ const tableName = burger;
 
 
 const orm = {
-    selectAll () {
-        console.log('select all');
+    selectAll: function () {
+        let queryString = "SELECT * FROM burger"
+        db.query(queryString, function (err, result){
+            if (err) throw err;
+            console.log(result);
+        })
     },
 
-    insertOne() {
-        console.log('insert one');
+    insertOne: function (values) {
+        let queryString = "INSERT INTO burger (burger_name, devoured) VALUES (?)";
+        db.query(queryString, [table, values], function (err, result){
+            if (err) throw err;
+            console.log(result);
+        })
     },
 
-    updateOne() {
-        console.log('update one');
+    updateOne(targBurg) {
+        let queryString = "UPDATE burger SET devoured = 1 WHERE burger_name = ??";
+        db.query(queryString, targBurg, function (err, result){
+            if (err) throw err;
+            console.log(result);
+        })
     }
 }
 
