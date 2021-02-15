d237abc6decbc04da03e8e30635f0f53b5cd5711
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-02-14 22:08:02 -0800

    create mysql connection.

---
+++ Config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "yourRootPassword",
+  database: "burger_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  module.exports = connection;
