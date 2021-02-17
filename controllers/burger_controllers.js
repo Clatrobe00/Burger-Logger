69ace3159efba43a28dda64eaaafecb5b4e3f218
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-02-17 10:38:37 -0800

    Write burger controller routing functions.

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,6 +1,30 @@
-const express = require('express');
-const burger = require('../models/burger')
+const burger = require('../models')
+const express = require("express");
 
+const router = express.Router();
 
 
 // create router for this app, export at end of file.
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        console.log('getting', data)
+    });
+});
+
+router.post("/", function (req, res){
+    burger.insert("Whopper", function(result) {
+        console.log('posting', result);
+    });
+});
+
+router.put("/", function (req, res){
+    burger.update("Big Mac", function(result){
+        console.log('updating', result);
+    });
+});
+
+
+
+
+module.exports = {router}
